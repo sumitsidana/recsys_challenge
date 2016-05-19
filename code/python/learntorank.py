03816f514246b865e134612e4c5d6a7d0ef7e90d
sumit <sumit.sidana@imag.fr>
2016-05-19 14:54:06 +0200

    update learn to rank

--- code/python/learntorank.py
+++ code/python/learntorank.py
@@ -4,9 +4,24 @@ mem = Memory("./mycache")
 
 @mem.cache
 def get_data():
-    data = load_svmlight_file("./inputFile_Ranking_libsvm")
+    data = load_svmlight_file("/home/ama/sidana/recommendersystemchallenge/output/input.similarity")
     return data[0], data[1]
 
 X, y = get_data()
-similarity = X.dot(X.T).todense()
 
+for i,row in enumerate(X):
+        for j,row in enumerate(X):
+                dotproduct = X[i].dot(X[j].T)
+                norm1 = X[i].dot(X[i].T)
+                norm2 = X[j].dot(X[j].T)
+                sqrtNorms = np.sqrt(norm1.dot(norm2))
+                print(dotproduct)
+                print(norm1)
+                print(norm2)
+                print(np.sqrt(norm1.dot(norm2)))
+                print(dotproduct/sqrtNorms)
+
+for i,row in enumerate(X):
+	for j,row in enumerate(X):
+		intersectV = np.intersect(X[i],X[j])
+		print(intersect)
